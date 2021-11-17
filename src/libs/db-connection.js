62c874af338dde3ea2490c9ba4cac0e08bd2b671
Mi5-hub <mino.randimbiarilanto@gmail.com>
2021-11-17 16:32:32 +0300

    test

--- src/libs/db-connection.js
+++ src/libs/db-connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const uri = "mongodb+srv://saynatest:@cluster0.rpee7.mongodb.net/crud-telm?retryWrites=true&w=majority"
 mongoose.connect("mongodb://localhost:27017/crud_db", 
     { useNewUrlParser: true, 
     useUnifiedTopology: true }
